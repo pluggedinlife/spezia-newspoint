bc0cbb5ede3562b85e9f3c141ef9a3339d1d9570
Mikhail Nazaretyan @pluggedinlife <sgas5570@gmail.com>
2024-01-16 20:12:49 +0100

    Enhance the code for testing purposes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log(`The server's up`);
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+require("dotenv").config();
+const token = process.env.BOT_TOKEN || "";
+
+const bot = new Telegraf(token);
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.on(message("text"), (ctx) => ctx.reply("Available"));
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
